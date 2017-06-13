a3615a8da1777f05fe0d957c473557fb79fb3e1a
aajayssingh <aajayssingh1@gmail.com>
2017-06-13 16:06:02 +0200

    trimming NVThreads to use for my apps

--- nvthreads-master/eval/parsec-3.0/defines.mk
+++
@@ -1,5 +0,0 @@
-NVTHREAD_HOME = $(PARSECDIR)/../..
-NVTHREAD_LIB = $(NVTHREAD_HOME)/src/libnvthread.so
-DTHREAD_LIB = $(NVTHREAD_HOME)/third-parties/dthreads/src/libdthread.so
-NVTHREAD_FLAGS = -rdynamic $(NVTHREAD_LIB) -ldl
-DTHREAD_FLAGS = -rdynamic $(DTHREAD_LIB) -ldl
--- nvthreads-master/eval/parsec-3.0/ext/defines.mk
+++
@@ -1,4 +0,0 @@
-SPLASH_HOME = $(PWD)/../../../..
-NVTHREAD_HOME = $(SPLASH_HOME)/../../../..
-NVTHREAD_LIB = $(NVTHREAD_HOME)/src/libnvthread.so
-DTHREAD_LIB = $(NVTHREAD_HOME)/third-parties/dthreads/src/libdthread.so
